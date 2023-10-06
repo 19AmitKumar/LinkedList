0b630c028bffcdc0ea590fb58158bc5827096c50
19AmitKumar <ak9978292@gmail.com>
2023-10-06 08:37:34 +0530

    [Amit] Add:Add Method of LinkedList

---
+++ LinkedList/LinkedList/LinkedListInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    //LinkedList is a linear Data structure
+    //It consist of group of nodes in a squence
+    //Data-Ecach node of a linkedlist store data
+    //Address-Each node of a LikedList contains the adress of next node,"next"
+    internal class LinkedListInfo
+    {
+        public Node head;
+        
+        public void Add(int data)
+        {
+            Node node=new Node(data);
+            if(head == null)
+            {
+                this.head=node;
+            }
+            else
+            {
+                Node temp=head;
+                while(temp.next!=null) 
+                { 
+                    temp=temp.next;
+                }
+                temp.next=node;
+            }
+            Console.WriteLine(node.data);
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListInfo listInfo = new LinkedListInfo();
+            listInfo.Add(56);
+            listInfo.Add(57);
+            listInfo.Add(58);
+            listInfo.Display();
+            Console.ReadLine();
+        }
+    }
+}
